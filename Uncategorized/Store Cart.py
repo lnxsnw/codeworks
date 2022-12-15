7e2cc5b07ed22c7cdba3028465003b7f797cf9c6
lnxsnw <113366745+lnxsnw@users.noreply.github.com>
2022-12-15 19:35:48 +0800

    Sanity Check v3

--- Uncategorized/Store Cart.py
+++ Uncategorized/Store Cart.py
@@ -1,262 +1,64 @@
-# Shopping Cart v2
+# Shopping Cart v3
 # Run this at https://www.onlinegdb.com/
-import os # For use of os.system('clear') funtion. This clears the screen!
-firstrun = 0
-# Here goes the shop sells
-products = ["Coffee", "Coke", "Pizza", "Cake", "Cookies"]
-prices = [10, 15, 25, 50, 7]
-# Here goes the customer picks
-s_products = []
-s_prices = []
+import os
+# Create a dictionary of food items and prices
+menu = {
+    "Coffee": 10,
+    "Coke": 15,
+    "Pizza": 25,
+    "Cake": 50,
+    "Cookies": 7
+}
 
-# Menu List
-def menu():
-    print ("We Have:")
-    print ("")
-    print (f"[1] {products[0]}  - P{prices[0]}")
-    print (f"[2] {products[1]}  - P{prices[1]}")
-    print (f"[3] {products[2]}  - P{prices[2]}")
-    print (f"[4] {products[3]}  - P{prices[3]}")
-    print (f"[5] {products[4]}  - P{prices[4]}")
-    print ("")
-    print ("Select your order by entering the number.")
-    print ("To order a product multiple times, simply re-enter it again!")
-    print ("To checkout, press 9. To cancel everything and exit, press 0.")
-    print ("")
+# Initialize a list to store the items the user picks
+order = []
 
-def checkout():
-    cartsize = len(s_prices)
+# Initialize a variable to store the total cost of the order
+total_cost = 0
+
+# Create a flag variable to track whether the user is done picking items
+done = False
+
+# Create a flag variable to track whether the user wants to checkout
+checkout = False
+
+# Loop until the user is done picking items
+while not done:
+    # Clear Screen and Print the menu
     os.system('clear')
-    print ("Name's Mini Store - Checkout")
-    print ("")
-    print (f"You currently have {cartsize} item(s) on your cart.")
-    print ("To Place Order, press 1. To Go Back, press 0.")
-    print ("")
-    print ("=============================================")
-    print ("            Your Cart Contains:             ")
-    print ("=============================================")
-    print ("")
-    print("Products    Price")
-    print ("")
-    for i in range(len(s_prices)):
-        print(s_products[i] + '\t    ' + s_prices[i])
-    print ("")
-    print ("=============================================")
-    print("             Total Price: P", sum(int(i) for i in s_prices))
-    print ("=============================================")
-    print ("")
-    while True: # Python has no switch case HAHAHAHA
-        addcartinput=input("Type in your selection:\n")
-        # Functions as a Switch Case (C/C++)
-        if (addcartinput==''):
-            os.system('clear')
-            print ("What?")
-            print ("")
-            checkout()
-        elif (addcartinput=='1'):
-            os.system('clear')
-            pay()
-        elif (addcartinput=='0'):
-            os.system('clear')
-            menu()
-        else:
-            os.system('clear')
-            print ("[ERR] Invalid Choice. Refreshing.")
-            print ("")
-            checkout()
+    print("Menu:")
+    for item, price in menu.items():
+        print("{}: ${}".format(item, price))
 
-def pay():
-    print ("Name's Mini Store - Payment")
-    print ("")
-    print("Total Price: P", sum(int(i) for i in s_prices))
-    payinput=input("Type in your amount to pay: P")
-    if (int(payinput) > sum(int(i) for i in s_prices)):
-        os.system('clear')
-        print ("Name's Mini Store - Checked Out!")
-        print ("")
-        print ("Transaction Complete!")
-        print ("Thank You for Shopping!")
-        print ("")
-        print ("=============================================")
-        print ("            You have checked out:             ")
-        print ("=============================================")
-        print ("")
-        print("Products    Price")
-        print ("")
-        for i in range(len(s_prices)):
-            print(s_products[i] + '\t    ' + s_prices[i])
-        print ("")
-        print ("=============================================")
-        print("             Total Price: P", sum(int(i) for i in s_prices))
-        print("       Total Amount Paid: P", int(payinput))
-        print ("=============================================")
-        print ("")
-        print ("Re-run the code to start again!")
-        exit()
-    elif (int(payinput) < sum(int(i) for i in s_prices)):
-        os.system('clear')
-        print ("Transaction Failed!")
-        print ("You have insufficent funds!")
-        print ("")
-        pay()
+    # Prompt the user to pick an item
+    choice = input("Choose an item: ")
+
+    # Add the item to the order and update the total cost
+    order.append(choice)
+    total_cost += menu[choice]
+
+    # Ask if the user wants to choose another item
+    another = input("Do you want to choose another item (y/n)? ")
+    if another.lower() == "n":
+        # If the user doesn't want to choose another item, set the "done" flag to True
+        done = True
+
+# Print the order and total cost
+os.system('clear')
+print("Your order: {}".format(order))
+print("Total cost: P{}".format(total_cost))
+
+# Ask if the user wants to checkout; Also repeat when customer has no money before and now has it
+checkout = input("Do you want to checkout (y/n)? ")
+if checkout.lower() == "y":
+    # If the user wants to checkout, clear screen and calculate the change
+    os.system('clear')
+    paid = float(input("Type in your amount to pay: P"))
+    if (int(paid) > int(total_cost)):
+        change = paid - total_cost
+        print("Your change is: P{:.2f}".format(change))
     else:
-        os.system('clear')
-        print ("Wait, What?")
-        print ("")
-
-# Frontcode
-print ("Welcome to Name's Mini Store!")
-print ("")
-menu()
-
-
-##### This is a mess but it works
-while True: # Python has no switch case HAHAHAHA
-    addcartinput=input("Type in your selection:\n")
-    # Functions as a Switch Case (C/C++)
-    if (addcartinput==''):
-        print ("What?")
-    elif (addcartinput=='0'):
-        os.system('clear')
-        print ("Name's Mini Store - Goodbye!")
-        print ("")
-        print ("Re-Run the program to restart it!")
-        exit()
-    elif (addcartinput=='1'):
-        os.system('clear')
-        print ("Name's Mini Store - Product Selection")
-        print ("")
-        print ("You have selected:")
-        print (f"[1] {products[0]}  - P{prices[0]}")
-        print ("")
-        ### Here is where customer will confirm the order or not
-        confirmselect=input("Press y to confirm, n to cancel selection and try again:\n")
-        if (confirmselect=='y'):
-            s_products.append(f'{products[0]}') # Add to Cart: Name
-            s_prices.append(f'{prices[0]}') # Add to Cart: Price
-            os.system('clear')
-            print (f'Hooray! Your {products[0]} has been added to the cart!')
-            print ("")
-            menu()
-        elif (confirmselect=='n'):
-            os.system('clear')
-            print (f'Your {products[0]} selection has been cancelled!')
-            print ("")
-            menu()
-        else:
-            os.system('clear')
-            print ("[ERR] Invalid Confirmation Choice. Restarting Menu.")
-            print ("")
-            menu()
-    elif (addcartinput=='2'):
-        os.system('clear')
-        print ("Name's Mini Store - Product Selection")
-        print ("")
-        print ("You have selected:")
-        print (f"[2] {products[1]}  - P{prices[1]}")
-        print ("")
-
-        confirmselect=input("Press y to confirm, n to cancel selection and try again:\n")
-        if (confirmselect=='y'):
-            s_products.append(f'{products[1]}')
-            s_prices.append(f'{prices[1]}')
-            os.system('clear')
-            print (f'Hooray! Your {products[1]} has been added to the cart!')
-            print ("")
-            menu()
-        elif (confirmselect=='n'):
-            os.system('clear')
-            print (f'Your {products[1]} selection has been cancelled!')
-            print ("")
-            menu()
-        else:
-            os.system('clear')
-            print ("[ERR] Invalid Confirmation Choice. Restarting Menu.")
-            print ("")
-            menu()
-    elif (addcartinput=='3'):
-        os.system('clear')
-        print ("Name's Mini Store - Product Selection")
-        print ("")
-        print ("You have selected:")
-        print (f"[3] {products[2]}  - P{prices[2]}")
-        print ("")
-
-        confirmselect=input("Press y to confirm, n to cancel selection and try again:\n")
-        if (confirmselect=='y'):
-            s_products.append(f'{products[2]}')
-            s_prices.append(f'{prices[2]}')
-            os.system('clear')
-            print (f'Hooray! Your {products[2]} has been added to the cart!')
-            print ("")
-            menu()
-        elif (confirmselect=='n'):
-            os.system('clear')
-            print (f'Your {products[2]} selection has been cancelled!')
-            print ("")
-            menu()
-        else:
-            os.system('clear')
-            print ("[ERR] Invalid Confirmation Choice. Restarting Menu.")
-            print ("")
-            menu()
-    elif (addcartinput=='4'):
-        os.system('clear')
-        print ("Name's Mini Store - Product Selection")
-        print ("")
-        print ("You have selected:")
-        print (f"[4] {products[3]}  - P{prices[3]}")
-        print ("")
-
-        confirmselect=input("Press y to confirm, n to cancel selection and try again:\n")
-        if (confirmselect=='y'):
-            s_products.append(f'{products[3]}')
-            s_prices.append(f'{prices[3]}')
-            os.system('clear')
-            print (f'Hooray! Your {products[3]} has been added to the cart!')
-            print ("")
-            menu()
-        elif (confirmselect=='n'):
-            os.system('clear')
-            print (f'Your {products[3]} selection has been cancelled!')
-            print ("")
-            menu()
-        else:
-            os.system('clear')
-            print ("[ERR] Invalid Confirmation Choice. Restarting Menu.")
-            print ("")
-            menu()
-    elif (addcartinput=='5'):
-        os.system('clear')
-        print ("Name's Mini Store - Product Selection")
-        print ("")
-        print ("You have selected:")
-        print (f"[5] {products[4]}  - P{prices[4]}")
-        print ("")
-
-        confirmselect=input("Press y to confirm, n to cancel selection and try again:\n")
-        if (confirmselect=='y'):
-            s_products.append(f'{products[4]}')
-            s_prices.append(f'{prices[4]}')
-            os.system('clear')
-            print (f'Hooray! Your {products[4]} has been added to the cart!')
-            print ("")
-            menu()
-        elif (confirmselect=='n'):
-            os.system('clear')
-            print (f'Your {products[4]} selection has been cancelled!')
-            print ("")
-            menu()
-        else:
-            os.system('clear')
-            print ("[ERR] Invalid Confirmation Choice. Restarting Menu.")
-            print ("")
-            menu()
-    elif (addcartinput=='9'):
-        os.system('clear')
-        checkout()
-    else:
-        os.system('clear')
-        print ("[ERR] Invalid Choice. Restarting Menu.")
-        print ("")
-        menu()
+        change = paid - total_cost
+        print("Oops! You are in short of P{:.2f}".format(change))
+else:
+    print("*Assumes you returned the items and left the store*")
